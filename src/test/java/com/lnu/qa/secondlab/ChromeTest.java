3584abe4fd9c39c3d3c4d80676ddbb275d2908b8
Mykyta Kostiuk <kostiuk.mykyta5@gmail.com>
2023-04-13 00:25:43 +0300

    [QA-2] Thread log

--- src/test/java/com/lnu/qa/secondlab/ChromeTest.java
+++ src/test/java/com/lnu/qa/secondlab/ChromeTest.java
@@ -3,6 +3,7 @@ package com.lnu.qa.secondlab;
 import com.lnu.qa.secondlab.mail.yahoo.tests.SendMessageTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,6 +15,7 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
+@Slf4j
 public class ChromeTest {
     private final static String PROPERTIES_PATH = "selenium.properties";
 
@@ -35,7 +37,7 @@ public class ChromeTest {
     public void firefoxSession() {
         WebDriverManager.chromedriver().setup();
         properties = loadProperties();
-        System.out.println(Thread.currentThread());
+        log.info("Thread: {}", Thread.currentThread());
     }
 
     @BeforeMethod
@@ -55,11 +57,11 @@ public class ChromeTest {
         return webDriverThreadLocal.get();
     }
 
-    public void sleep(){
+    public void sleep() {
         sleep(Integer.parseInt(properties.get("tests.sleep.time").toString()));
     }
 
-    public void sleep(int seconds){
+    public void sleep(int seconds) {
         TestUtils.sleep(Duration.ofSeconds(seconds));
     }
 
